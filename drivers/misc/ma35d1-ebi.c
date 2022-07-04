d4709c0f1a520f42a646004f22e52a706974e2c1
schung <schung@nuvoton.com>
2022-07-04 11:21:38 +0800

    Fixd MCLKDIV not setting via device tree
    Change-Id: I78030fbfff744edc1633be071c3e8439ccbed0c6

--- drivers/misc/ma35d1-ebi.c
+++ drivers/misc/ma35d1-ebi.c
@@ -258,6 +258,11 @@ static int ma35d1_ebi_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "EBI Timing control 'IDLE' cannot be read!\n");
 		return ret;
 	}
+	ret = of_property_read_u32_array(pdev->dev.of_node,"MCLKDIV",&ebi[ch]->MCLKDIV, 1);
+	if (ret) {
+		dev_err(&pdev->dev, "EBI Timing control 'MCLKDIV' cannot be read!\n");
+		return ret;
+	}
 
 	ma35d1_set_ebi_ctl(ebi[ch]->bank);
 
