5136a33939e744037b879e3a7ec35a92f537a4d7
Jacky Huang <ychuang3@nuvoton.com>
2023-12-05 18:57:01 +0800

    Add OP-TEE support for MA35D0/H0 TRNG driver
    Add OP-TEE client driver option for MA35D0 and MA35H0
    TRNG driver.
    Also, update ma35d0_defconfig and ma35h0_defconfig to
    add TEE and OP-TEE.
    
    Change-Id: Ifadeadb93e6e32ad1d40b1c013828dae56a9b334

--- drivers/char/hw_random/ma35h0-trng.c
+++ drivers/char/hw_random/ma35h0-trng.c
@@ -20,11 +20,12 @@
 #include <linux/platform_device.h>
 #include <linux/completion.h>
 #include <linux/of_platform.h>
+#include <linux/tee_drv.h>
 
 //#define USE_GEN_NONCE
 
 /*--------------------------------------------------------------*/
-/*  MA35H0 TRNG registers                                      */
+/*  MA35h0 TRNG registers                                      */
 /*--------------------------------------------------------------*/
 #define CTRL			0x000
 #define CTRL_CMD_OFFSET			(0)
@@ -129,6 +130,277 @@ struct ma35h0_trng {
 #define TCMD_ZEROIZE		0xf /* Zeroize                    */
 
 #define TRNG_TIMEOUT		(2000)
+
+#ifdef CONFIG_OPTEE
+
+#define SHARE_MEM_SIZE		4096
+
+/*-------------------------------------------------------------------*/
+/*  OP-TEE client driver for MA35H0 secure TRNG                     */
+/*-------------------------------------------------------------------*/
+/*
+ * PTA_CMD_TRNG_INIT - Initialize TRNG hardware
+ *
+ * param[0] (out value) - value.a: TRNG STAT register
+ *                        value.b: TRNG ISTAT register
+ * param[1] unused
+ * param[2] unused
+ * param[3] unused
+ *
+ * Result:
+ * TEE_SUCCESS - Invoke command success
+ * TEE_ERROR_TRNG_BUSY - TRNG hardware busy
+ * TEE_ERROR_TRNG_GEN_NOISE - Failed to generate noise or nounce
+ * TEE_ERROR_TRNG_COMMAND - TRNG command failed
+ */
+#define PTA_CMD_TRNG_INIT		0x1
+
+/*
+ * PTA_CMD_TRNG_READ - Get TRNG data
+ *
+ * param[0] (inout memref) - TRNG data buffer memory reference
+ * param[1] unused
+ * param[2] unused
+ * param[3] unused
+ *
+ * Result:
+ * TEE_SUCCESS - Invoke command success
+ * TEE_ERROR_BAD_PARAMETERS - Incorrect input param
+ */
+#define PTA_CMD_TRNG_READ		0x2
+
+#define RND_DATA_SHM_SZ		(4 * 1024)
+
+/**
+ * struct optee_rng_private - OP-TEE private data of MA35H0 TRNG
+ * @dev:		OP-TEE based RNG device.
+ * @ctx:		OP-TEE context handler.
+ * @session_id:		RNG TA session identifier.
+ * @rdata_shm_pool:	Memory pool shared with OP-TEE MA35H0 TRNG
+ * @optee_rng:		Linux hwrng device data
+ */
+struct optee_rng_private {
+	struct device *dev;
+	struct tee_context *ctx;
+	u32 session_id;
+	struct tee_shm *rdata_shm_pool;
+	struct hwrng optee_rng;
+};
+
+#define to_optee_rng_private(r) \
+		container_of(r, struct optee_rng_private, optee_rng)
+
+static size_t get_optee_rng_data(struct optee_rng_private *pvt_data,
+				 void *buf, size_t req_size)
+{
+	int ret = 0;
+	u8 *rng_data = NULL;
+	size_t rng_size = 0;
+	struct tee_ioctl_invoke_arg inv_arg;
+	struct tee_param param[4];
+
+	memset(&inv_arg, 0, sizeof(inv_arg));
+	memset(&param, 0, sizeof(param));
+
+	/* Invoke PTA_CMD_TRNG_READ function of Trusted App */
+	inv_arg.func = PTA_CMD_TRNG_READ;
+	inv_arg.session = pvt_data->session_id;
+	inv_arg.num_params = 4;
+
+	/* Fill invoke cmd params */
+	param[0].attr = TEE_IOCTL_PARAM_ATTR_TYPE_MEMREF_INOUT;
+	param[0].u.memref.shm = pvt_data->rdata_shm_pool;
+	param[0].u.memref.size = req_size;
+	param[0].u.memref.shm_offs = 0;
+
+	ret = tee_client_invoke_func(pvt_data->ctx, &inv_arg, param);
+	if ((ret < 0) || (inv_arg.ret != 0)) {
+		dev_err(pvt_data->dev, "PTA_CMD_TRNG_READ invoke err: %x\n",
+			inv_arg.ret);
+		return 0;
+	}
+
+	rng_data = tee_shm_get_va(pvt_data->rdata_shm_pool, 0);
+	if (IS_ERR(rng_data)) {
+		dev_err(pvt_data->dev, "tee_shm_get_va failed\n");
+		return 0;
+	}
+
+	rng_size = param[0].u.memref.size;
+	memcpy(buf, rng_data, rng_size);
+
+	return rng_size;
+}
+
+static int optee_rng_read(struct hwrng *rng, void *buf, size_t max, bool wait)
+{
+	struct optee_rng_private *pvt_data = to_optee_rng_private(rng);
+	size_t read = 0, rng_size = 0;
+	u8 *data = buf;
+
+	if (max > RND_DATA_SHM_SZ)
+		max = RND_DATA_SHM_SZ;
+
+	if (max % 4)
+		return -EINVAL;
+
+	while (read == 0) {
+		rng_size = get_optee_rng_data(pvt_data, data, (max - read));
+
+		data += rng_size;
+		read += rng_size;
+	}
+	return read;
+}
+
+static int optee_rng_init(struct hwrng *rng)
+{
+	struct optee_rng_private *pvt_data = to_optee_rng_private(rng);
+	struct tee_shm *rdata_shm_pool = NULL;
+
+	rdata_shm_pool = tee_shm_alloc(pvt_data->ctx, RND_DATA_SHM_SZ,
+					 TEE_SHM_MAPPED | TEE_SHM_DMA_BUF);
+	if (IS_ERR(rdata_shm_pool)) {
+		dev_err(pvt_data->dev, "tee_shm_alloc failed\n");
+		return PTR_ERR(rdata_shm_pool);
+	}
+
+	pvt_data->rdata_shm_pool = rdata_shm_pool;
+
+	return 0;
+}
+
+static void optee_rng_cleanup(struct hwrng *rng)
+{
+	struct optee_rng_private *pvt_data = to_optee_rng_private(rng);
+
+	tee_shm_free(pvt_data->rdata_shm_pool);
+}
+
+static struct optee_rng_private pvt_data = {
+	.optee_rng = {
+		.name		= "optee-nvt-trng",
+		.init		= optee_rng_init,
+		.cleanup	= optee_rng_cleanup,
+		.read		= optee_rng_read,
+	}
+};
+
+static int ma35h0_optee_trng_init(struct device *dev)
+{
+	int ret = 0;
+	struct tee_ioctl_invoke_arg inv_arg;
+	struct tee_param param[4];
+
+	memset(&inv_arg, 0, sizeof(inv_arg));
+	memset(&param, 0, sizeof(param));
+
+	/* Invoke PTA_CMD_TRNG_INIT function of Trusted App */
+	inv_arg.func = PTA_CMD_TRNG_INIT;
+	inv_arg.session = pvt_data.session_id;
+	inv_arg.num_params = 4;
+
+	/* Fill invoke cmd params */
+	param[0].attr = TEE_IOCTL_PARAM_ATTR_TYPE_VALUE_OUTPUT;
+
+	ret = tee_client_invoke_func(pvt_data.ctx, &inv_arg, param);
+	if ((ret < 0) || (inv_arg.ret != 0)) {
+		dev_err(dev, "PTA_CMD_TRNG_INIT invoke err: %x\n",
+			inv_arg.ret);
+		return -EINVAL;
+	}
+
+	pr_info("optee get TRNG STAT=0x%llx, ISTAT=0x%llx\n",
+			param[0].u.value.a, param[0].u.value.b);
+
+	return 0;
+}
+
+static int optee_ctx_match(struct tee_ioctl_version_data *ver, const void *data)
+{
+	if (ver->impl_id == TEE_IMPL_ID_OPTEE)
+		return 1;
+	else
+		return 0;
+}
+
+static int optee_rng_probe(struct device *dev)
+{
+	struct tee_client_device *rng_device = to_tee_client_device(dev);
+	int ret = 0, err = -ENODEV;
+	struct tee_ioctl_open_session_arg sess_arg;
+
+	memset(&sess_arg, 0, sizeof(sess_arg));
+
+	/* Open context with TEE driver */
+	pvt_data.ctx = tee_client_open_context(NULL, optee_ctx_match, NULL,
+					       NULL);
+	if (IS_ERR(pvt_data.ctx))
+		return -ENODEV;
+
+	/* Open session with hwrng Trusted App */
+	memcpy(sess_arg.uuid, rng_device->id.uuid.b, TEE_IOCTL_UUID_LEN);
+	sess_arg.clnt_login = TEE_IOCTL_LOGIN_PUBLIC;
+	sess_arg.num_params = 0;
+
+	ret = tee_client_open_session(pvt_data.ctx, &sess_arg, NULL);
+	if ((ret < 0) || (sess_arg.ret != 0)) {
+		dev_err(dev, "TRNG tee_client_open_session failed, err: %x\n",
+			sess_arg.ret);
+		err = -EINVAL;
+		goto out_ctx;
+	}
+	pvt_data.session_id = sess_arg.session;
+
+	err = ma35h0_optee_trng_init(dev);
+	if (err)
+		goto out_sess;
+
+	err = hwrng_register(&pvt_data.optee_rng);
+	if (err) {
+		dev_err(dev, "optee hwrng registration failed (%d)\n", err);
+		goto out_sess;
+	}
+
+	pvt_data.dev = dev;
+
+	return 0;
+
+out_sess:
+	tee_client_close_session(pvt_data.ctx, pvt_data.session_id);
+out_ctx:
+	tee_client_close_context(pvt_data.ctx);
+
+	return err;
+}
+
+static int optee_rng_remove(struct device *dev)
+{
+	hwrng_unregister(&pvt_data.optee_rng);
+	tee_client_close_session(pvt_data.ctx, pvt_data.session_id);
+	tee_client_close_context(pvt_data.ctx);
+
+	return 0;
+}
+
+static const struct tee_client_device_id optee_rng_id_table[] = {
+	{UUID_INIT(0x9f831ffa, 0x1823, 0x4ee9,
+		   0x8f, 0xb2, 0x41, 0x57, 0x1f, 0x64, 0x32, 0xe1)},
+	{}
+};
+
+MODULE_DEVICE_TABLE(tee, optee_rng_id_table);
+
+static struct tee_client_driver optee_rng_driver = {
+	.id_table	= optee_rng_id_table,
+	.driver		= {
+		.name		= "optee-nvt-trng",
+		.bus		= &tee_bus_type,
+		.probe		= optee_rng_probe,
+		.remove		= optee_rng_remove,
+	},
+};
+#endif  /* CONFIG_OPTEE */
 
 /*-------------------------------------------------------------------*/
 /*  hwrng driver for MA35H0 TRNG                                    */
@@ -321,6 +593,19 @@ static int ma35h0_trng_probe(struct platform_device *pdev)
 	struct resource		*res;
 	int			err;
 
+#ifdef CONFIG_OPTEE
+	const char  *optee_sel;
+	bool  optee_nuvoton = false;
+
+	if (!of_property_read_string(dev->of_node, "optee_nuvoton", &optee_sel)) {
+		if (!strcmp("yes", optee_sel))
+			optee_nuvoton = true;
+	}
+	if (optee_nuvoton == true) {
+		pr_info("Register MA35H0 TRNG optee client driver.\n");
+		return driver_register(&optee_rng_driver.driver);
+	}
+#endif
 	tdev = devm_kzalloc(dev, sizeof(*tdev), GFP_KERNEL);
 	if (!tdev)
 		return -ENOMEM;
