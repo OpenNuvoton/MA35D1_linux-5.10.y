25040783f6bd07d91d647afc085618784e1c55bd
mjchen <mjchen@nuvoton.com>
2025-02-17 10:30:38 +0800

    Update CAN driver for MA35 series version B SoC
      - Added MA35 version check code to distinguish different MA35 series SoC versions.
      - For the "read register workaround", version B directly reads the register
        without applying the workaround, while version A remains unchanged.
      - Removed the "TX must wait for bus idle" restriction for version B, while
        version A remains unchanged.
    
    Change-Id: I1a9d947a74c5d76f8a1dc95e5b5023f8ca70702b

--- drivers/net/can/m_can/m_can.h
+++ drivers/net/can/m_can/m_can.h
@@ -98,6 +98,13 @@ struct m_can_classdev {
 	struct mram_cfg mcfg[MRAM_CFG_NUM];
 };
 
+struct m_can_plat_priv {
+	void __iomem *base;
+	void __iomem *mram_base;
+	spinlock_t	lock;
+	u32		version;
+};
+
 struct m_can_classdev *m_can_class_allocate_dev(struct device *dev);
 void m_can_class_free_dev(struct net_device *net);
 int m_can_class_register(struct m_can_classdev *cdev);
